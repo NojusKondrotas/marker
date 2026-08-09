99d1ea52c3f38ae953343badf1727320b9af956d
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-08-09 12:02:30 +0300

    Create basic root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import NotePreview from "@/components/app/note-preview/NotePreview.svelte";
+
+    const notes = [];
+    for (let i = 1; i <= 6; ++i) {
+        notes.push({id: i, content: "India Lima Uniform Victor Yankee Oscar Uniform", titles: [{title: "Mike", id: 1}, {title: "November", id: 2}, {title: "Oscar", id: 3}]});
+    }
+</script>
+
+<main class="h-screen pl-[10vw] py-[10vh] overflow-hidden gap-x-40 gap-y-20 flex flex-col flex-wrap content-start">
+    {#each notes as note (note.id)}
+        <NotePreview {...note} style=4 />
+    {/each}
+</main>
