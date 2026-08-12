bf21eff2056a640ae25c7f20fe4845725eb33969
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-08-12 11:05:36 +0300

    Opt in for built-in scrollbar hiding

--- src/routes/note/[note]/[mark]/+layout.svelte
+++ src/routes/note/[note]/[mark]/+layout.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex flex-col items-center w-screen h-screen">
 	<div class="w-full h-[min(10%,50px)] shrink-0"></div>
-	<main class="w-[min(40%,600px)] text-2xl leading-[2.8] tracking-wider scrollbar-hidden overflow-y-auto">
+	<main class="w-[min(40%,600px)] text-2xl leading-[2.8] tracking-wider no-scrollbar overflow-y-auto">
 		{@render children()}
 	</main>
 	<div class="w-full h-[min(10%,50px)] shrink-0"></div>
