583bd16315a2b21292cebbf8870a8a8795385a16
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-09-13 11:43:57 +0300

    Don't truncate highlight agent contents

--- src/lib/components/app/note-preview/menus/highlights/NotePreviewHighlightAgent.svelte
+++ src/lib/components/app/note-preview/menus/highlights/NotePreviewHighlightAgent.svelte
@@ -8,5 +8,5 @@
 
 <div class="flex flex-col">
     <NotePreviewHighlightAgentOffset offset={props.offset}></NotePreviewHighlightAgentOffset>
-    <p class="text-lg max-w-[16ch] cursor-default text-ellipsis overflow-hidden whitespace-nowrap">{props.contents}</p>
+    <p class="text-lg cursor-default overflow-x-hidden text-nowrap w-full min-w-0">{props.contents}</p>
 </div>
